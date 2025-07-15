38428c40ee66cdcdf39e59cfb0a192226e1aae39
kumardeepanshu2413@gmail.com <kumardeepanshu2413@gmail.com>
2025-07-15 10:00:34 +0530

    fixes

--- BackEnd/src/controllers/ai.controller.js
+++ BackEnd/src/controllers/ai.controller.js
@@ -6,12 +6,10 @@ module.exports.getReview = async (req, res) => {
     const code = req.body.code;
 
     if (!code) {
-        return res.status(400).send("Prompt is required");
+    return res.status(400).send("Prompt is required");
     }
 
     const response = await aiService(code);
-
-
     res.send(response);
 
 }
